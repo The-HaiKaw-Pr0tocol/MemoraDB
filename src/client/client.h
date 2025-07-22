a014c626a86af1d65bf86ba278564ebae475e9de
Weasel <116844401+kei077@users.noreply.github.com>
2025-07-22 21:57:53 +0000

    chore: move client files to client folder

---
+++ src/client/client.h
@@ -0,0 +1,47 @@
+/**
+ * =====================================================
+ * MemoraDB - In-Memory Database System
+ * =====================================================
+ * 
+ * File                      : src/utils/client.h
+ * Module                    : Client Utilities Header
+ * Last Updating Author      : sch0penheimer
+ * Last Update               : 07/19/2025
+ * Version                   : 1.0.0
+ * 
+ * Description:
+ *   Header file for MemoraDB TCP client utilities.
+ * 
+ * 
+ * Copyright (c) 2025 MemoraDB Project
+ * =====================================================
+ */
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+/* ==================== CONSTANTS ==================== */
+#define SERVER_PORT         6379
+#define BUFFER_SIZE         1024
+#define DEFAULT_SERVER_IP   "127.0.0.1"
+#define MAX_ARGS           64
+
+/* ==================== STRUCTURES ==================== */
+typedef struct {
+    int socket_fd;
+    struct sockaddr_in server_addr;
+    const char *server_ip;
+    int port;
+    int connected;
+} client_t;
+
+#endif
